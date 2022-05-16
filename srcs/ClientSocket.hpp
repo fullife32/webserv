88f04182f07be095d9e8287652794659195796ec
Eithan ASSOULINE <eassouli@e2r12p2.clusters.42paris.fr>
2022-05-16 19:18:40 +0200

    moving functions, add functions to accept new clients, creating a multiplex class for epoll functions and client class and add commentaries

---
+++ srcs/ClientSocket.hpp
@@ -0,0 +1,45 @@
+#ifndef CLIENT_SOCKET_HPP
+# define CLIENT_SOCKET_HPP
+
+#include <sys/socket.h>
+#include <unistd.h>
+#include <sys/epoll.h>
+
+/*
+	This class contains all informations needed to create and work with a
+	client socket, to send and recv packages. It basically contains its fd, the
+	client datas received with accept() and the events waited.
+*/
+class ClientSocket {
+private:
+	int					_fd;
+	sockaddr_storage 	_cli;
+	epoll_event			_event;
+
+public:
+	ClientSocket(int fd, sockaddr_storage cli) : _fd(fd), _cli(cli) {
+		_event.events = EPOLLIN;
+		_event.data.fd = _fd;
+	}
+	~ClientSocket() {}
+	// ClientSocket( ClientSocket const &other );
+	// ClientSocket &operator=( ClientSocket const &other );
+
+	/* Close socket */
+	void	close() {
+		::close(_fd);
+	}
+
+	/* Getter functions */
+	int	getFd() const {
+		return _fd;
+	}
+	epoll_event	*getEvent() {
+		return &_event;
+	}
+	uint32_t	getEventValue() const {
+		return _event.events;
+	}
+};
+
+#endif
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,30 +1,31 @@
 #include <iostream>
-#include "serverSocket.hpp"
+#include "ServerSocket.hpp"
+#include "ParseConfig.hpp"
+#include "Multiplex.hpp"
 
-int	plexSocket(serverSocket socket);
+int	plexSocket(ServerSocket socket);
 
+/*
+	Starting by gathering config file infos, then creating each socket
+	associated with a server block and the multiplexing everything (adding an
+	event list and waiting for all of them at the same time).
+*/
 int	main(int ac, char **av) {
-	int	epfd; //don't forget to close it
-
 	if (ac != 3) {
 		std::cout << "./webserv ip port" << std::endl;
 		return 1;
 	}
-	serverBlock		datas(av[1], av[2]);
-	serverSocket	serverTest(datas);
 
-	epfd = plexSocket(serverTest);
-	while(1) {
-		int ready;
-		epoll_event waitList;
-		waitList.events = EPOLLIN;
-		ready = epoll_wait(epfd, &waitList, 10, -1);
-		if (ready == -1) {
-			std::cout << "epoll_wait failed" << std::endl;
-			break;
-		}
-		std::cout << "Finished waiting" << std::endl;
-		std::cout << serverTest.getEvent() << std::endl;
+	ServerBlock		datas(av[1], av[2]); // not finished
+	ServerSocket	serverTest(datas);
+	Multiplex		plex;
+
+	plex.addToPoll(serverTest.getFd(), serverTest.getEvent());  //loop through all server sockets
+	for (;;) {
+		serverTest.showInfos();
+		if (plex.waitPlex() == -1)
+			exit(1); // don't exit
+		plex.watchEvents(serverTest);
 	}
 	serverTest.close();
 }
