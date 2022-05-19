76d7a13580b279946644accf455efe914f84f6d5
Eithan ASSOULINE <eassouli@e2r9p4.clusters.42paris.fr>
2022-05-19 19:11:54 +0200

    add struct to parsing

--- includes/Config/ParseConfig.hpp
+++ includes/Config/ParseConfig.hpp
@@ -6,7 +6,7 @@
 /*   By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 11:14:39 by eassouli          #+#    #+#             */
-/*   Updated: 2022/05/19 15:16:25 by eassouli         ###   ########.fr       */
+/*   Updated: 2022/05/19 16:21:25 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,26 +27,37 @@
 */
 
 struct s_location {
-
-};
-
-struct s_server {
-	std::string							workers;
+	int									body_size;
 	std::string							address; // if no address bind 0.0.0.0
 	std::string							port; // if no port bind 80
 	std::vector<std::string>			server_names;
+	std::vector<std::string>			allowed_methods;
 	std::string							root;
 	std::string							auth[2];
 	std::map<int, std::string>			error_pages; // error path
 	std::string							upload;
 	bool								autoindex; // default off/false
 	std::vector<std::string>			indexes; // default index.html ?
-	std::map<std::string, std::string>	extensions;
-	std::string							cgiFolder;
+	std::map<std::string, std::string>	cgis;
+	std::string							cgi_bin; // default ./cgi-bin
+};
+
+struct s_server {
+	size_t								body_size;
+	std::string							address; // if no address bind 0.0.0.0
+	std::string							port; // if no port bind 80
+	std::vector<std::string>			server_names;
+	std::string							root;
+	std::map<int, std::string>			error_pages; // error path
+	// std::string							upload;
+	bool								autoindex; // default off/false
+	std::vector<std::string>			indexes; // default index.html ?
+	std::map<std::string, std::string>	cgis;
+	std::string							cgi_bin; // default ./cgi-bin
 	std::vector<struct s_location>		locations;
 };
 
-// when get one info check if exist or if default possible like
+// when get one info check if exist or if default possible like cgi folder or autoindex
 
 
 class ParseConfig {
@@ -64,3 +75,20 @@ public:
 };
 
 #endif
+
+server {
+	// server_name default;
+	listen localhost:80 default
+	location /truc {
+		no right
+	}
+}
+
+server {
+	// server_name google.com;
+	listen localhost:80;
+	
+	location /truc {
+		all rights
+	}
+}
