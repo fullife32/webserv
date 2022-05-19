c760e0b3c1867572a739cde7a18a75b72ee481ec
Eithan ASSOULINE <eassouli@e1r3p5.clusters.42paris.fr>
2022-05-19 15:16:52 +0200

    preparing server structure

--- includes/Config/ParseConfig.hpp
+++ includes/Config/ParseConfig.hpp
@@ -6,7 +6,7 @@
 /*   By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 11:14:39 by eassouli          #+#    #+#             */
-/*   Updated: 2022/05/18 11:14:54 by eassouli         ###   ########.fr       */
+/*   Updated: 2022/05/19 15:16:25 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,41 +15,52 @@
 
 #include <string>
 #include <cstdlib>
+#include <vector>
+#include <map>
+
+#define AUTH_LOGIN 0
+#define AUTH_PASS 1
 
 /*
 	Those classes will have all infos of one server block received from the
 	config file.
 */
-class LocBlock {
-private:
-	
-public:
-	LocBlock() {}
-	~LocBlock() {}
-	// LocBlock( LocBlock const &other );
-	// LocBlock &operator=( LocBlock const &other );
+
+struct s_location {
+
 };
 
-class ServerBlock {
+struct s_server {
+	std::string							workers;
+	std::string							address; // if no address bind 0.0.0.0
+	std::string							port; // if no port bind 80
+	std::vector<std::string>			server_names;
+	std::string							root;
+	std::string							auth[2];
+	std::map<int, std::string>			error_pages; // error path
+	std::string							upload;
+	bool								autoindex; // default off/false
+	std::vector<std::string>			indexes; // default index.html ?
+	std::map<std::string, std::string>	extensions;
+	std::string							cgiFolder;
+	std::vector<struct s_location>		locations;
+};
+
+// when get one info check if exist or if default possible like
+
+
+class ParseConfig {
 	std::string	_ipAddr;
 	std::string	_listen;
 	// std::string server_name;
 	// std::vector<std::string[2]> listen;
 	// std::vector<std::string> index;
-	// list of LocBlock;
+	ParseConfig() {}
+	ParseConfig( ParseConfig const &other );
+	ParseConfig &operator=( ParseConfig const &other );
 public:
-	ServerBlock() {}
-	ServerBlock(std::string ip, std::string port) : _ipAddr(ip), _listen(port) {}
-	~ServerBlock() {}
-	// ServerBlock( ServerBlock const &other );
-	// ServerBlock &operator=( ServerBlock const &other );
-
-	const char *getIp() const {
-		return _ipAddr.c_str();
-	}
-	int	getPort() const {
-		return atoi(_listen.c_str());
-	}
+	ParseConfig( std::string configPath ) {}
+	~ParseConfig() {}
 };
 
 #endif
